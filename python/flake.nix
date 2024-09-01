6b3b4af65a3149d126cb2531271c24896fe48c05
Kogara13 <kieranogara@gmail.com>
2024-09-01 10:03:34 -0400

    created templates for python and default shell

---
+++ python/flake.nix
@@ -0,0 +1,31 @@
+# flake.nix
+
+{
+  description = "Python development flake";
+
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+		python3
+            ];
+          };
+	  packages = [
+	    (pkgs.python3.withPackages (python-pkgs: [
+		# put python packages here
+		# Example: python-pkgs.pandas
+	    ]))
+	  ];
+    };
+}
